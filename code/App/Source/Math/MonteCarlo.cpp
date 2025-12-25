5b242151a3be7760cfa51b9b6ae860ec5dd01dec
Karla Falcão <karlapsfalcao@gmail.com>
2025-12-25 18:26:20 -0300

    fix: define M_PI for MSVC compatibility

--- code/App/Source/Math/MonteCarlo.cpp
+++ code/App/Source/Math/MonteCarlo.cpp
@@ -1,5 +1,11 @@
 #include "MonteCarlo.h"
+
+#define _USE_MATH_DEFINES
 #include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
 // Static members initialization
 std::mt19937 MonteCarlo::generator;
